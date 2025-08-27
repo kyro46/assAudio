2277ec646826e41cf1a534a453bb002e3e717a2a
kyro46 <cjobst@wifa.uni-leipzig.de>
2025-08-27 13:10:50 +0200

    Fix Mantis 45674 - Can't copy questionpool with assAudio-question inside

--- classes/class.assAudio.php
+++ classes/class.assAudio.php
@@ -272,7 +272,7 @@ class assAudio extends assQuestion
         // make a real clone to keep the object unchanged
         $clone = clone $this;
         
-        $original_id = assQuestion::_getOriginalId($this->getId());
+        $original_id = $this->questioninfo->getOriginalId($this->id);
         $source_questionpool_id = $this->getObjId();
         $clone->setId(-1);
         $clone->setObjId($target_questionpool_id);
